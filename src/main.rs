e74d71a1be8f8ca79997b236fab35341505e011e
nitzel <jan.s.92@web.de>
2021-02-19 23:08:19 +0000

    Add doc comments; Format code

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@
 use arrayvec::ArrayVec;
 use board_game_traits::board::{Board as BoardTrait, Color, GameResult};
 use pgn_traits::pgn::PgnBoard;
-use std::{cmp::Ordering, iter, str::FromStr};
-use taik::{board, board::{Board, Direction, Move, Movement, Role, StackMovement}};
-use serde::{Serialize};
+use serde::Serialize;
 use serde_json;
+use std::{cmp::Ordering, iter, str::FromStr};
+use taik::{
+    board,
+    board::{Board, Direction, Move, Movement, Role, StackMovement},
+};
 
 pub fn parse_move<const S: usize>(input: &str) -> board::Move {
     let words: Vec<&str> = input.split_whitespace().collect();
@@ -31,18 +34,19 @@ pub fn parse_move<const S: usize>(input: &str) -> board::Move {
 
         let mut pieces_held = num_pieces_taken;
 
-        let pieces_taken: ArrayVec<[Movement; board::MAX_BOARD_SIZE - 1]> = iter::once(num_pieces_taken)
-            .chain(
-                pieces_dropped
-                    .iter()
-                    .take(pieces_dropped.len() - 1)
-                    .map(|pieces_to_drop| {
-                        pieces_held -= pieces_to_drop;
-                        pieces_held
-                    }),
-            )
-            .map(|pieces_to_take| Movement { pieces_to_take })
-            .collect();
+        let pieces_taken: ArrayVec<[Movement; board::MAX_BOARD_SIZE - 1]> =
+            iter::once(num_pieces_taken)
+                .chain(
+                    pieces_dropped
+                        .iter()
+                        .take(pieces_dropped.len() - 1)
+                        .map(|pieces_to_drop| {
+                            pieces_held -= pieces_to_drop;
+                            pieces_held
+                        }),
+                )
+                .map(|pieces_to_take| Movement { pieces_to_take })
+                .collect();
 
         let direction = match (
             start_square.rank::<S>().cmp(&end_square.rank::<S>()),
@@ -67,7 +71,6 @@ pub fn parse_move<const S: usize>(input: &str) -> board::Move {
     }
 }
 
-
 fn parse_server_notation<const S: usize>(server_notation: &str) -> Vec<Move> {
     let move_splits = server_notation.split(",");
     move_splits.map(parse_move::<S>).collect()
@@ -76,12 +79,18 @@ fn parse_server_notation<const S: usize>(server_notation: &str) -> Vec<Move> {
 fn do_it<const S: usize>(server_notation: &str) {
     let moves = parse_server_notation::<S>(server_notation);
     println!("SRV: {}", server_notation);
-    println!("PTN: {:?}", moves.iter().map(|m| m.to_string::<S>()).collect::<Vec<String>>());
+    println!(
+        "PTN: {:?}",
+        moves
+            .iter()
+            .map(|m| m.to_string::<S>())
+            .collect::<Vec<String>>()
+    );
 
     // Apply moves
     let mut position = Board::<S>::start_board();
     for (_i, ply) in moves.iter().enumerate() {
-        println!("Ply{} {}",_i, ply.to_string::<S>());
+        println!("Ply{} {}", _i, ply.to_string::<S>());
         position.do_move(ply.clone());
     }
 
@@ -95,10 +104,9 @@ fn do_it<const S: usize>(server_notation: &str) {
 }
 
 fn main() {
-/*
-P A1,P G1,P D4,P C4,P D3,P C3,P D5,P E4 C,P C5,P B5,P B4 C,P E3,P E5,M E4 D4 1,P F5,P D6,P G5,P A5,P B6,P C6,P A4,M B5 C5 1,P B5 C,M C5 D5 2,M B5 C5 1,M D4 D5 2,P D4,P E4,P F4,P E2,M C5 C4 1,M D5 D4 2,P C5 W,M D5 E5 3,P D5 W,P E6 C,M D5 E5 1,M E6 E5 1,P D5 W,P E6,P F3 W,P D2,M F3 E3 1,P E7,M C4 C3 2,P E1,P F6 W,P B1,P C1,P C2,P B2,P A2,P B5,P B7,P A7,P A6,P B3,P C7,M C3 C2 3,M A6 A7 1,P A6,M D4 D3 3,M F6 E6 1,M A5 A6 1,P D7,P A5 W,P G6,P G7,P F7,P F6,P F3,P D4,M D7 C7 1,M C6 C7 1,P D7 W,M E5 D5 1,P C6 W,M D5 E5 2,M D7 C7 1,M A5 B5 1,P D7,P D5,M C6 D6 1,P F2,P G2,P G3,P G4,M E5 F5 7,M F7 G7 1,M F5 G5 3,P E5 W,P A3,M E5 F5 1,P E5 W,M F5 F2 3 1 2,M E4 F4 1,M F3 F4 1,P E4 W,M F4 F7 1 2 2,M E5 F5 1,M E6 F6 2,P E5,P E6,P C3,M C7 A7 2 2,M B5 B6 2,P C7,M B6 B7 3,M D6 D7 2,M G5 G6 4,P A5,P B6,M A7 A6 3,M B7 A7 5,P C6,P B5,P D6,M G6 G7 5,M D7 E7 2,M G7 F7 6,M F6 E6 5,M F7 F6 7,P G5,M F6 E6 1,M F2 E2 3,M F5 F6 2,P G6
-*/
-
+    /*
+    P A1,P G1,P D4,P C4,P D3,P C3,P D5,P E4 C,P C5,P B5,P B4 C,P E3,P E5,M E4 D4 1,P F5,P D6,P G5,P A5,P B6,P C6,P A4,M B5 C5 1,P B5 C,M C5 D5 2,M B5 C5 1,M D4 D5 2,P D4,P E4,P F4,P E2,M C5 C4 1,M D5 D4 2,P C5 W,M D5 E5 3,P D5 W,P E6 C,M D5 E5 1,M E6 E5 1,P D5 W,P E6,P F3 W,P D2,M F3 E3 1,P E7,M C4 C3 2,P E1,P F6 W,P B1,P C1,P C2,P B2,P A2,P B5,P B7,P A7,P A6,P B3,P C7,M C3 C2 3,M A6 A7 1,P A6,M D4 D3 3,M F6 E6 1,M A5 A6 1,P D7,P A5 W,P G6,P G7,P F7,P F6,P F3,P D4,M D7 C7 1,M C6 C7 1,P D7 W,M E5 D5 1,P C6 W,M D5 E5 2,M D7 C7 1,M A5 B5 1,P D7,P D5,M C6 D6 1,P F2,P G2,P G3,P G4,M E5 F5 7,M F7 G7 1,M F5 G5 3,P E5 W,P A3,M E5 F5 1,P E5 W,M F5 F2 3 1 2,M E4 F4 1,M F3 F4 1,P E4 W,M F4 F7 1 2 2,M E5 F5 1,M E6 F6 2,P E5,P E6,P C3,M C7 A7 2 2,M B5 B6 2,P C7,M B6 B7 3,M D6 D7 2,M G5 G6 4,P A5,P B6,M A7 A6 3,M B7 A7 5,P C6,P B5,P D6,M G6 G7 5,M D7 E7 2,M G7 F7 6,M F6 E6 5,M F7 F6 7,P G5,M F6 E6 1,M F2 E2 3,M F5 F6 2,P G6
+    */
 
     // println!("{:?}", parse_move::<5>("M A1 A3 23"));
     // println!("{:?}", parse_move::<5>("M A1 A3 23").to_string::<5>());
@@ -142,11 +150,11 @@ P A1,P G1,P D4,P C4,P D3,P C3,P D5,P E4 C,P C5,P B5,P B4 C,P E3,P E5,M E4 D4 1,P
 
     // input contains a road with tinue at least for the last 3 plies
     let input = "a1 e1 e2 a4 e3 d3 e4 e5 d5 e5- Cd4 c1 e3< 2e4-11"; // e3-"; // e3"; // d2";
-    // https://ptn.ninja/NoZQlgXgpgBARAVjgXQLAChRgC6zgB2wDsA6IsIgKwEMUNgARa3eAJgAZWBGEzkrgOx1MAFTABbPBwBcXABzSEQtJgBKUAM4BXADbZ4qgLTthGHjGoIYAIwAsGViRgBjAMw2EGV04Amtl-botk4gPu4Awj6e6AhOUO5Q0QBsTuFQ-lGGGAK+rBasGHJO1FwuBegAnE7WeT5cZuzVpXUA1Gbm1lwtZQA8Zo42CeVc3oOGLtFcwTaliX3oXLEWXTBRAHxmKcswUKxZCzk2tt0g1q5mRTC2NWtcXHlQXWZVO67jIM716BzVeUbsQA&name=KwD2AIAoCYEog&ply=38!
-    // Tinue starts here (7 ply deep)
+                                                                    // https://ptn.ninja/NoZQlgXgpgBARAVjgXQLAChRgC6zgB2wDsA6IsIgKwEMUNgARa3eAJgAZWBGEzkrgOx1MAFTABbPBwBcXABzSEQtJgBKUAM4BXADbZ4qgLTthGHjGoIYAIwAsGViRgBjAMw2EGV04Amtl-botk4gPu4Awj6e6AhOUO5Q0QBsTuFQ-lGGGAK+rBasGHJO1FwuBegAnE7WeT5cZuzVpXUA1Gbm1lwtZQA8Zo42CeVc3oOGLtFcwTaliX3oXLEWXTBRAHxmKcswUKxZCzk2tt0g1q5mRTC2NWtcXHlQXWZVO67jIM716BzVeUbsQA&name=KwD2AIAoCYEog&ply=38!
+                                                                    // Tinue starts here (7 ply deep)
     let input = "a5 b4 c3 b5 d4 c4 Sd3 Cd5 e3 e5 Ce4 d5- d2 a2 a1 c2 b2 d1 b1 d1+ b1+ c2< b3 e2 b3- c5 b1 e5< a1+ d5> a1 e2-";
     let input = "a5 b4 c3 b5 d4 c4 Sd3 Cd5 e3 e5 Ce4 d5- d2 a2 a1 c2 b2 d1 b1 d1+ b1+ c2< b3 e2 b3- c5 b1 e5< a1+ d5> a1 e2- b4+ Sb3 4b2>112 e1+ e3- Sc1 b2";
-    let input = "a5 b4 c3 b5 d4 c4 Sd3 Cd5 e3 e5 Ce4 d5- d2 a2 a1 c2 b2 d1 b1 d1+ b1+ c2< b3 e2 b3- c5 b1 e5< a1+ d5> a1 e2- b4+ Sb3";// 4b2>112 e1+ e3- Sc1 b2";
+    let input = "a5 b4 c3 b5 d4 c4 Sd3 Cd5 e3 e5 Ce4 d5- d2 a2 a1 c2 b2 d1 b1 d1+ b1+ c2< b3 e2 b3- c5 b1 e5< a1+ d5> a1 e2- b4+ Sb3"; // 4b2>112 e1+ e3- Sc1 b2";
     let input = "a5 b4 c3 b5 d4 c4 Sd3 Cd5 e3 e5 Ce4 d5- d2 a2 a1 c2 b2 d1 b1 d1+ b1+ c2< b3 e2 b3- c5 b1 e5< a1+ d5> a1 e2-"; // b4+ Sb3";// 4b2>112 e1+ e3- Sc1 b2";
     let moves_till_tinue = 5;
     let mut position = <Board<5>>::start_board();
@@ -157,19 +165,12 @@ P A1,P G1,P D4,P C4,P D3,P C3,P D5,P E4 C,P C5,P B5,P B4 C,P E3,P E5,M E4 D4 1,P
     let me = Color::White; // position.side_to_move();
     println!("\n// Tinue in up to {} plies as {}: ", moves_till_tinue, me);
     let winning_moves = win_in_n(&mut position, moves_till_tinue, me, true);
-    
-
 
     print_tinue_moves_json(&winning_moves);
-    println!("\n\\\\###\n\n{}", serde_json::to_string(&tms_to_trs_me(&winning_moves)).unwrap());
-
-    // for mvs in win_in_n(&mut position, moves_till_tinue, me) {
-    //     for mv in mvs {
-    //         print!("{}  ", position.move_to_san(&mv));
-    //     }
-    //     println!("");
-    // }
-    // println!();
+    println!(
+        "{}",
+        serde_json::to_string(&tinuemove_to_options(&winning_moves)).unwrap()
+    );
 }
 
 /// Returns every move that immediately wins the game
@@ -196,68 +197,76 @@ fn win_in_one<const S: usize>(position: &mut Board<S>) -> Vec<Vec<Move>> {
 
 type Mov = String;
 
+/// A structure that summarises multiple `TinueMove`s into a single `struct`.
+///
+/// If the winning replies (called `solutions`) to a set of `moves` are the same
+/// then they are grouped into a single `TinueMoveOptions` instance.
+/// # TLDR
+/// Basically: If **Player A** plays one of `moves`, then **Player B** must play
+///            one of `solutions` to stay on the **Road to Tinue**.
 #[derive(Serialize, Debug, Eq, PartialEq, Clone)]
-struct TR {
+struct TinueMoveOptions {
+    /// Possible moves
     moves: Vec<Mov>,
+    /// Responses applicable to any of `moves` that stay on the **Road to Tinue**.
     #[serde(skip_serializing_if = "<[_]>::is_empty")]
-    solutions: Vec<TR>,
+    solutions: Vec<TinueMoveOptions>,
 }
 
-
-fn tms_to_trs_me(tmvs: &Vec<TinueMove>) -> Vec<TR> {
-    // mvs.iter().map(|mv|
-    //     match mv.next {
-    //         Some(next) => tm_to_tr(next),
-    //         None => Tr { }
-    //     }
-    // )
-
-    let trs: Vec<(Mov, Option<Vec<TR>>)> = tmvs.iter().map(|tm| (
-        tm.mv.clone(),
-        match &tm.next {
-            Some(next) => Some(tms_to_trs_me(&next)),
-            None => None
-        }
-    )).collect();
+/// Reduces `TinueMove`s to `TinueMoveOption`s
+fn tinuemove_to_options(tmvs: &Vec<TinueMove>) -> Vec<TinueMoveOptions> {
+    let trs: Vec<(Mov, Option<Vec<TinueMoveOptions>>)> = tmvs
+        .iter()
+        .map(|tm| {
+            (
+                tm.mv.clone(),
+                match &tm.next {
+                    Some(next) => Some(tinuemove_to_options(&next)),
+                    None => None,
+                },
+            )
+        })
+        .collect();
 
     let groups = trs.group_by(|(_, next1), (_, next2)| next1 == next2);
 
-    groups.map(|group| TR {
-        moves: group.iter().map(|(mv, _)| mv.clone()).collect::<Vec<String>>(),
-        solutions: match group.first() {
-            None => vec![],
-            Some((_, solution)) => solution.clone().unwrap_or(vec![])
-        }
-         //.map(|(_, solution)| )
-            // .iter()
-            // .filter(|(_, solution)| solution.is_some())
-            // .flat_map(|(_, solution)| solution.clone().unwrap())
-            // .collect::<Vec<TR>>()
-    }).collect()
+    groups
+        .map(|group| TinueMoveOptions {
+            moves: group
+                .iter()
+                .map(|(mv, _)| mv.clone())
+                .collect::<Vec<String>>(),
+            solutions: match group.first() {
+                None => vec![],
+                Some((_, solution)) => solution.clone().unwrap_or(vec![]),
+            },
+        })
+        .collect()
 }
 
-// fn tm_to_tr(mv: &TinueMove) -> TR {
-//     if let Some(next) = mv.next {
-//         let next_trs = tm_to_tr(&next);
-//     }
-//     TR {
-//         mv: mv.mv,
-//         next: None,
-//     }
-// }
-
-
+/// Represents a `Move` on the **Road to Tinue** and possible responses (`next`)
 struct TinueMove {
     mv: Mov,
+    /// When `mv` is played, any of these responses will stay on the **Road to Tinue**
     next: Option<Vec<TinueMove>>,
 }
 
+/// Prints `TinueMove`s as a JSON object.
+/// ### Example
+/// ```
+/// {
+/// "b4+": {
+///   "4b2<": {
+///     "a5": { "4a2+112": {}, },
+///     "Sa5": { "5a2>1112": {}, },
+/// ...
+/// ```
 fn print_tinue_moves_json(moves: &Vec<TinueMove>) {
     fn print_tinue_move(mv: &TinueMove, depth: usize) {
         println!("{}\"{}\": {{", "  ".repeat(depth), mv.mv);
         if let Some(next) = &mv.next {
             for next_move in next {
-                print_tinue_move(&next_move, depth+1);
+                print_tinue_move(&next_move, depth + 1);
             }
         }
         println!("{}}},", "  ".repeat(depth))
@@ -270,10 +279,26 @@ fn print_tinue_moves_json(moves: &Vec<TinueMove>) {
     println!("}}");
 }
 
-// Todo introduce my_color:Color so that we can start with the opponent making the first move as well
-// This will also help to identify early wins caused by the opponent
-// Todo can we return early once we've found a tinue by ourselves? We don't need all of them
-fn win_in_n<const S: usize>(position: &mut Board<S>, depth: u32, me: Color, find_only_one_tinue: bool) -> Vec<TinueMove> {
+/// Returns all **Roads to Tinue** for player `me`
+/// that are available at `position`.
+///
+/// `depth`: Number of plies to look into the future
+///
+/// `find_only_one_tinue`: If `true` returns only the first **Road to Tinue**
+///
+/// #### Remarks
+/// If `depth` is high, this may still return sub-optimal Tinues
+/// where the opponent is not blocking the Tinue and thus `me` can waste another move.
+///
+/// #### Caution
+/// This method becomes very slow very quickly. `depth=5`, maybe `7` is recommended.
+/// Best to set `find_only_one_tinue=true`.
+fn win_in_n<const S: usize>(
+    position: &mut Board<S>,
+    depth: u32,
+    me: Color,
+    find_only_one_tinue: bool,
+) -> Vec<TinueMove> {
     let mut legal_moves = vec![];
     let mut tinue_moves = vec![];
 
@@ -285,31 +310,39 @@ fn win_in_n<const S: usize>(position: &mut Board<S>, depth: u32, me: Color, find
         let reverse_move = position.do_move(mv.clone());
         if let Some(result) = position.game_result() {
             // Early win or loss
-            if my_turn
-            {
+            if my_turn {
                 if result == GameResult::WhiteWin && me == Color::White
-                || result == GameResult::BlackWin && me == Color::Black {
+                    || result == GameResult::BlackWin && me == Color::Black
+                {
                     if find_only_one_tinue {
                         position.reverse_move(reverse_move);
-                        return vec![TinueMove { mv: position.move_to_san(&mv), next: None }];
+                        return vec![TinueMove {
+                            mv: position.move_to_san(&mv),
+                            next: None,
+                        }];
                     }
-                    tinue_moves.push(TinueMove { mv: position.move_to_san(&mv), next: None })
+                    tinue_moves.push(TinueMove {
+                        mv: position.move_to_san(&mv),
+                        next: None,
+                    })
                 }
-            }
-            else {
+            } else {
                 // Early loss or draw
                 // TODO: Actually, this could be an early road/flatwin if that's the only possible enemy move
                 //       So we should add checks for that
                 position.reverse_move(reverse_move);
                 return vec![];
             }
-        }
-        else if depth > 1 {
+        } else if depth > 1 {
             let winning_moves = win_in_n(position, depth - 1, me, find_only_one_tinue);
-            if my_turn { // I play
+            if my_turn {
+                // I play
                 if !winning_moves.is_empty() {
                     // This move leads to Tinue
-                    let this_move = TinueMove{ mv: position.move_to_san(&mv), next: Some(winning_moves)};
+                    let this_move = TinueMove {
+                        mv: position.move_to_san(&mv),
+                        next: Some(winning_moves),
+                    };
 
                     if find_only_one_tinue {
                         position.reverse_move(reverse_move);
@@ -317,16 +350,19 @@ fn win_in_n<const S: usize>(position: &mut Board<S>, depth: u32, me: Color, find
                     }
                     tinue_moves.push(this_move)
                 }
-            }
-            else { // opponent plays
+            } else {
+                // opponent plays
                 if winning_moves.is_empty() {
                     // Because the opponet play `mv` doesn't lead to Tinue,
                     // this entire branch is not on the road to Tinue.
                     position.reverse_move(reverse_move);
-                    return vec![]; 
+                    return vec![];
                 }
                 // This and the previous opponent moves are on the road to Tinue so add it
-                let this_move = TinueMove{ mv: position.move_to_san(&mv), next: Some(winning_moves)};
+                let this_move = TinueMove {
+                    mv: position.move_to_san(&mv),
+                    next: Some(winning_moves),
+                };
                 tinue_moves.push(this_move)
             }
         }
